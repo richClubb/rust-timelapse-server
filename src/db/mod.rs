a824d89e6e2f319ee0ba4b0aa603ff3277dbd935
Richard Clubb <rich.clubb@hidglobal.com>
2025-02-11 15:38:37 +0000

    added trace to db files

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,11 @@
 use influxdb2::Client;
 use influxdb2::models::DataPoint;
 
+use opentelemetry::global::ObjectSafeSpan;
+use opentelemetry::Context;
+use opentelemetry::{global, trace::Tracer};
+use opentelemetry::trace::{SpanKind, Status};
+
 use futures::prelude::*;
 
 #[derive(Clone)]
@@ -23,7 +28,16 @@ impl Database {
         Ok(())
     }
 
+    #[allow(dead_code)]
     pub async fn add_entry_vec8(&self, camera: &str, path: String) -> Result<(), Box<dyn std::error::Error>> {
+
+        let parent_cx = Context::current();
+        let tracer = global::tracer("rust-timelapse-server");
+
+        let mut span = tracer
+            .span_builder(format!("add to db vec8"))
+            .with_kind(SpanKind::Server)
+            .start_with_context(&tracer, &parent_cx);
 
         let data = vec![DataPoint::builder("pictures")
                                 .tag("camera", camera)
@@ -31,6 +45,8 @@ impl Database {
                                 .build()?];
 
         self.client.write("pictures", stream::iter(data)).await?;
+
+        span.set_status(Status::Ok);
 
         Ok(())
     }
